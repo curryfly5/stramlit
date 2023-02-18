4d86266487f9d136fae1c35d0a799adcb69b619c
curryfly5 <101237486+curryfly5@users.noreply.github.com>
2023-02-18 15:32:54 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,8 +1,30 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
 
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
-st.table(dataframe)
+# Create main function
+def main():
+    st.sidebar.header("Navigation")
+    page = st.sidebar.selectbox("Choose a page", 
+                                ["Home", "About", "Contact"])
+    if page == "Home":
+        home()
+    elif page == "About":
+        about()
+    elif page == "Contact":
+        contact()
+
+# Create page functions
+def home():
+    st.title("Homepage")
+    st.write("This is the content of the Homepage")
+
+def about():
+    st.title("About")
+    st.write("This is the content of the About page")
+
+def contact():
+    st.title("Contact")
+    st.write("This is the content of the Contact page")
+
+# Run main function
+if __name__ == "__main__":
+    main()
