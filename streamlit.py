9c71598f516253e6ccf64461af020d352511b356
curryfly5 <101237486+curryfly5@users.noreply.github.com>
2023-02-18 15:17:24 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,8 +1,8 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
-st.dataframe(dataframe.style.highlight_max(axis=0))
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+st.table(dataframe)
